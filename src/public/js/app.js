1c9293f8769908532dbb4d5e5e8004a3a958f857
basalcode <basalcode@gmail.com>
2021-11-09 21:40:33 +0900

    Create chat feature

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,25 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
 });
 
-socket.addEventListener("message", (message) => {
-    console.log("Just got this:", message.data, "from the server");
+socket.addEventListener("message", async (message) => {
+    console.log(await message.data.text());
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!!");
-}, 5000);
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+
+    input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -15,16 +15,17 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 and w
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", socket => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", () => {
         console.log("Disconnected from Browser ❌");
     });
     socket.on("message", message => {
-        console.log(message.toString('utf-8'));
-
+        sockets.forEach((aSocket) => aSocket.send(message));
     });
-    socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen)
