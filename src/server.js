174726d5309c6ff17f3795a4bb8c936ddd9e9b88
basalcode <basalcode@gmail.com>
2021-11-15 22:55:07 +0900

    Create room count feature

--- src/server.js
+++ src/server.js
@@ -16,6 +16,22 @@ const handleListen = () => console.log(`Listening on http://localhost:3000 and w
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny(event => {
