9c9b39affb03a50b71fe74492963169ee5ba1db0
basalcode <basalcode@gmail.com>
2021-11-21 22:09:28 +0900

    Implement camera select box

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,24 +13,40 @@ async function getCameras() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+
         cameras.forEach(camera => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option)
         })
     } catch (e) {
         console.log(e);
     }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+    const initialConstrinats = {
+        audio: true,
+        video: { facingMode: "user" },
+    }
+    const cameraConstraints = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    }
+
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true,
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrinats
+        );
         myFace.srcObject = myStream;
-        await getCameras();
+        if (!device) {
+            await getCameras();
+        }
     } catch (e) {
         console.log(e);
     }
@@ -60,5 +76,10 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
+camerasSelect.addEventListener("input", handleCameraChange)
