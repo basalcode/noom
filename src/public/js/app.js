f631823de5b695aa723afb721f443532b56e8e2a
basalcode <basalcode@gmail.com>
2021-11-22 21:58:20 +0900

    Prepare for webRTC

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,14 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
     try {
@@ -82,4 +86,26 @@ async function handleCameraChange() {
 
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
-camerasSelect.addEventListener("input", handleCameraChange)
+camerasSelect.addEventListener("input", handleCameraChange);
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia() {
+    welcome.hidden = true;
+    call.hidden = false;
+    getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+socket.on("welcome", () => {
+    console.log("someone joined");
+})
